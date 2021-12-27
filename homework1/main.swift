e5cca38173d937491ea359b4eb01b24c4edbb14e
Larisa <larisavalgutova@gmail.com>
2021-12-27 14:58:05 +0300

    Задача 1

--- homework1/main.swift
+++ homework1/main.swift
@@ -7,6 +7,30 @@
 
 import Foundation
 
-print("Hello, World!")
-let a = 4
-let b = 3
+// Задача 1. Решить квадратное уравнение.
+
+let a = 2.0
+let b = 5.0
+let c = -7.0
+var x1 = 0.0
+var x2 = 0.0
+
+
+let disc = b*b - 4*a*c
+
+
+if disc < 0 {
+    print("Корней нет")
+}
+else if disc == 0 {
+    x1 = -b/(2*a)
+}
+else {
+    x1 = (-b - sqrt(disc))/(2*a)
+    x2 = (-b + sqrt(disc))/(2*a)
+}
+
+print(disc)
+print(x1)
+print(x2)
+
