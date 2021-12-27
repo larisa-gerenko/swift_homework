9b29e4ce61424d61e746656f338321d5b4817aa3
Larisa <larisavalgutova@gmail.com>
2021-12-27 15:30:46 +0300

    Задача 3

--- homework1/main.swift
+++ homework1/main.swift
@@ -12,8 +12,8 @@ import Foundation
 //let a = 2.0
 //let b = 5.0
 //let c = -7.0
-//var x1 = 0.0
-//var x2 = 0.0
+//let x1 = 0.0
+//let x2 = 0.0
 //
 //
 //let disc = b*b - 4*a*c
@@ -36,14 +36,29 @@ import Foundation
 
 // Задача 2. Даны катеты прямоугольного треугольника. Найти площадь, периметр и гипотенузу треугольника.
 
-let leg1 = 20.0
-let leg2 = 30.0
+//let leg1 = 20.0
+//let leg2 = 30.0
+//
+//let hypo = sqrt((pow(leg1, 2))+(pow(leg2, 2)))
+//let square = (1/2)*(leg1*leg2)
+//let perimeter = leg1 + leg2 + hypo
+//
+//
+//print(hypo)
+//print(square)
+//print(perimeter)
 
-let hypo = sqrt((pow(leg1, 2))+(pow(leg2, 2)))
-let square = (1/2)*(leg1*leg2)
-let perimeter = leg1 + leg2 + hypo
 
+// Задача 3. Пользователь вводит сумму вклада в банк и годовой процент. Найти сумму вклада через 5 лет.
 
-print(hypo)
-print(square)
-print(perimeter)
+let summ = 350000
+let percent = 11
+let years = 5
+let days = years*365
+
+let perEnd = (summ * percent * (days/365))/100
+let summEnd = summ + perEnd
+
+print(perEnd)
+print(summEnd)
+
