6495d64fad35743aa53dfe2dd6b09e1e3ea0d4ab
Larisa <larisavalgutova@gmail.com>
2021-12-30 13:25:13 +0300

    Task2

--- homework1/main.swift
+++ homework1/main.swift
@@ -13,7 +13,7 @@ import Foundation
 //Практическое задание 2
 //1. Написать функцию, которая определяет, четное число или нет.
 
-func isEven (number: Int) {   
+func isEven (number: Int) {
     if number % 2 == 0 {
         print("Число четное")
     } else {
@@ -23,3 +23,14 @@ func isEven (number: Int) {
 
 var result: () = isEven(number: 21)
 
+//2. Написать функцию, которая определяет, делится ли число без остатка на 3.
+
+func fallIntoThree (numberTwo: Int) {
+    if numberTwo % 3 == 0 {
+        print("Число делится без остатка на 3")
+    } else {
+        print("Число не делится без остатка на 3")
+    }
+}
+
+var resultTwo: () = fallIntoThree(numberTwo: 20)
