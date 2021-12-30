7a0c6ea2625c999bd616c2356d86f6388fe6e2a3
Larisa <larisavalgutova@gmail.com>
2021-12-30 17:28:41 +0300

    delete mistakes2

--- homework1/main.swift
+++ homework1/main.swift
@@ -10,27 +10,27 @@ import Foundation
 //Практическое задание 2
 //1. Написать функцию, которая определяет, четное число или нет.
 
-func isEven (_ number: Int) {
-    if number % 2 == 0 {
-        print("Число четное")
-    } else {
-        print("Число нечетное")
-    }
+func isEven (_ number: Int) -> Bool {
+    return number % 2 == 0
 }
 
-isEven(21)
+if isEven(21) {
+    print("Число четное")
+} else {
+    print("Число нечетное")
+}
 
 //2. Написать функцию, которая определяет, делится ли число без остатка на 3.
 
-func fallIntoThree (_ numberTwo: Int) {
-    if numberTwo % 3 == 0 {
-        print("Число делится без остатка на 3")
-    } else {
-        print("Число не делится без остатка на 3")
-    }
+func fallIntoThree (_ numberTwo: Int) -> Bool{
+    return numberTwo % 3 == 0
 }
 
-fallIntoThree(20)
+if fallIntoThree(20) {
+    print("Число делится без остатка на 3")
+} else {
+    print("Число не делится без остатка на 3")
+}
 
 // 3. Создать возрастающий массив из 100 чисел.
 
@@ -39,7 +39,8 @@ print(array)
 
 // 4. Удалить из этого массива все четные числа и все числа, которые не делятся на 3.
 
-var deleteNumbers = array.filter {$0 % 2 != 0 && $0 % 3 == 0}
+var deleteNumbers = array.filter { !isEven($0) && fallIntoThree($0) }
+
 print(deleteNumbers)
 
 
